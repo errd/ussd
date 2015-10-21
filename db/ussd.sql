274276a8a543a538a9997855c3b1f9fd1e46c87f
errd <skonyshev@gmail.com>
2015-10-21 23:55:41 +0300

    Initial commit

---
+++ db/ussd.sql
@@ -0,0 +1,23 @@
+create table if not exists ussd_codes (
+    code text primary key,
+    val numeric not null,
+    status int not null default 0,
+    created long not null,
+    updated long not null
+);
+
+create table if not exists sim_cards (
+    num text primary key,
+    val numeric not null,
+    active int not null default 1,
+    created long not null,
+    updated long not null
+);
+
+create table if not exists ussd_log (
+    created long not null,
+    code text,
+    num text,
+    response text,
+    status int
+);
