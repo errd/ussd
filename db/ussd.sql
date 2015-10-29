b030b40ddca780dd14559aa6190bdc948b164f2c
errd <skonyshev@gmail.com>
2015-10-30 01:36:20 +0300

    syncronize module experiments

--- db/ussd.sql
+++ db/ussd.sql
@@ -2,14 +2,11 @@ create table if not exists ussd_codes (
     code long primary key,
     val numeric not null,
     num long,
-    status int not null default 0,
-    created long not null,
-    updated long
+    status int not null default 0
 );
 
 create table if not exists sim_cards (
     num long primary key,
-    val numeric not null,
     active int not null default 1,
     created long not null,
     updated long
@@ -19,6 +16,7 @@ create table if not exists ussd_log (
     created long not null,
     code text,
     num text,
+    val numeric not null,
     response text,
     status int
 );
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var fs = require("fs");
 var xlsx = require('node-xlsx');
 var multiparty = require('multiparty');
+var sync = require('synchronize')
+var fiber = require('fibers');
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'USSD Express' });
@@ -150,20 +152,63 @@ router.post('/upload-ussd', function(req, res, next) {
     form.parse(req);
 });
 
+router.get('/assign', function(req, res, next) {
+    assignSimcard();
+    res.send('OK');
+});
+
+function getUnassignedCount(cb) {
+    db.all("select count(*) as cnt from ussd_codes where num is null", function (err, row) {
+        cb(null, row[0].cnt);
+    })
+}
+
+function getSimcardValue(num, from, to, cb) {
+    db.all("select sum(val) as val from ussd_log where num='" + num + "' and created between " + from + " and " + to, function (err, rows) {
+        if(rows && rows[0] && rows[0].val != null) cb(null, rows[0].val); else cb(null, 0);
+    })
+}
+
+function getMonthStart(num, cb) {
+    db.all("select sum(val) as val from ussd_log where num='" + num + "' and created between " + from + " and " + to, function (err, row) {
+        cb(null, row[0].val);
+    })
+}
+
+function assignSimcard() {
+    var daylyLimit = 1000;
+    var monthlyLimit = 30000;
+    fiber(function() {
+        if(sync(getUnassignedCount)() < 1) return;
+
+        var valueFunc = sync(getSimcardValue);
+        console.log(valueFunc);
+        console.log(valueFunc('9', 0, 1));
+
+
+        var date = new Date();
+        var dayStart = date.setHours(0, 0, 0, 0);
+        var dayEnd = date.setHours(23, 59, 59, 999);
+        var monthStart = (new Date(date.getFullYear(), date.getMonth(), 1)).setHours(0, 0, 0, 0);
+        var monthEnd = (new Date(date.getFullYear(), date.getMonth() + 1, 0)).setHours(23, 59, 59, 999);
+        db.each("select * from sim_cards order by num", function (err, row) {
+            //console.log(valueFunc('9', 0, 1));
+            //var dayValue = valueFunc(row.num, dayStart, dayEnd);
+            //console.log("Day value = " + dayValue);
+            //if(sync(getSimcardValue, row.num, dayStart, dayEnd)() >= daylyLimit) return;
+            //if(sync(getSimcardValue)(row.num, monthStart, monthStart + 30*24*3600*1000) >= monthlyLimit) return;
+        });
+    }).run();
+}
+assignSimcard();
+
+/*db.all("select
+ sc.num,
+ sum(uc.val) as tval
+ from sim_cards sc
+ left join ussd_codes uc on sc.num=uc.num and uc.updated between 0 and 4
+ where sc.num='9'
+ group by sc.num
+ ")*/
+
 module.exports = router;
-
-/*function assignSimcard() {
-    var simcards = [];
-    var dailyLimit = 10000;
-    var monthlyLimit = 40000;
-
-    db.all("select
-    sc.num,
-    sum(uc.val) as tval
-    from sim_cards sc
-    left join ussd_codes uc on sc.num=uc.num and uc.updated between 0 and 4
-    where sc.num='9'
-    group by sc.num
-
-    ")
-}*/
